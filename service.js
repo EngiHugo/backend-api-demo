a04ae0c7874c71f55e7a2694cb6c0d236e35d0e1
hugo <hbernal31@gmail.com>
2022-11-22 21:03:10 -0500

    se implemento la insercion de registros en la base de datos de Mongo

--- service.js
+++ service.js
@@ -10,7 +10,30 @@ app.get('/', function (req, res) {
 // POST method route
 app.post('/register', (req, res) => {
     console.log(req.body)
-    res.json(req.body)
+    const modelshema =  {
+        "name":"string",
+        "lastname":"string",
+        "email":"string",
+        "password":"string",
+        "password_confirmation":"string"
+    }
+    const schema = new mongoose.Schema(modelshema);
+    const users = mongoose.model('users', schema);
+    const user = new users({
+        'name':req.body.name,
+        'lastname':req.body.lastname,
+        'email':req.body.email,
+        'password':req.body.password,
+
+    })
+    user.save((error)=>{
+        if(error !== null){
+            console.log("error de registro de usuario",error)
+        }
+       
+        res.json(user)
+    })
+    
 }) 
 mongoose.connect(DB_URI).then((client)=>{
     console.log("Conexion a base de datos satisfactoria")
